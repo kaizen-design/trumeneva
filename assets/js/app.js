5696d9aae87d4e69733fdf64b9d7b3bc4194a5ca
kaizen-design <denis@kaizen-design.ru>
2024-01-23 16:04:10 +0300

    Hero section completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,33 @@
 const APP = {
-  init: function() {
-    //  Prevent jump to top on "#" links
-    document.querySelectorAll("a[href='#']").forEach(link => {
-      link.addEventListener("click", e => e.preventDefault());
-    });
+  init: () => {
+    APP.handleDeadLinks();
+    APP.scrollToAnchor();
   },
   
-  scrollTop: function() {
+  handleDeadLinks: () => {
+    document.querySelectorAll("a[href='#']").forEach($link => {
+      $link.addEventListener("click", e => e.preventDefault());
+    });
+  },
+
+  scrollToTop: () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  },
+
+  scrollToAnchor: ($el = document, offset = 0) => {
+    $el.querySelectorAll('.smoothScroll').forEach($link => {
+      $link.addEventListener("click", function(event) {
+        event.preventDefault();
+        const href = this.getAttribute("href"), 
+              $el = document.querySelector(href); 
+        if ($el) {
+          window.scrollTo({ top: $el.offsetTop + offset, behavior: "smooth" });
+        }
+      });
+    });
   }
 };
+
 (function() {
   APP.init();
 })();
