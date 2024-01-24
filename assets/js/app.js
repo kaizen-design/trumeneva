78adb7ef33257b0b6f4b6083d2af4b4337a0c9ea
kaizen-design <denis@kaizen-design.ru>
2024-01-25 00:56:58 +0300

    Countdown addded

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,10 @@ const APP = {
     // Common
     APP.handleDeadLinks();
     APP.scrollToAnchor();
+    APP.initCountDown(
+      document.querySelector(".site-alert .counter"),
+      new Date().setHours(new Date().getHours() + 4) // TODO: pass correct value
+    );
 
     // Homepage
     APP.initSlider('.specialists-slider');
@@ -134,9 +138,46 @@ const APP = {
 
   },
 
+  initCountDown: ($el, date) => {
+    
+    if (!$el && !date) return;
+    
+    // Set the date we're counting down to
+    const countDownDate = new Date(date).getTime();
+
+    // Update the count down every 1 second
+    const x = setInterval(function() {
+
+      // Get today's date and time
+      const now = new Date().getTime();
+        
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now;
+        
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+      // Output the result in an element with id="demo"
+      $el.textContent = 
+        (days ? days + ":" : '') + 
+        (hours ? hours + ":" : '') + 
+        (minutes ? minutes + ":" : '') + 
+        (seconds ? seconds : '');
+        
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        $el.textContent = "Время вышло!";
+      }
+    }, 1000);
+  }
+
 };
 
-function initAccordion() {
+function countDown() {
   
 }
 
